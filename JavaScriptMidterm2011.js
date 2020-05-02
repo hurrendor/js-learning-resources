52011aa708a29502d06c8c950840f4c46ff8771d
Stacy <hurrendor@gmail.com>
2020-05-02 18:41:12 -0500

    Add midterm resource

---
+++ JavaScriptMidterm2011.js
@@ -0,0 +1,31 @@
+/**Prompt the user for their age, and using 4 complex logical expressions (and, or; NOT if/else), output whether the user is:
+  * Too young to drive, vote or drink alcohol
+  * Old enough to drive, but not vote or drink
+  * Old enough to drive and vote, but not drink
+  * Old enough to do all three
+**/
+
+
+/**
+*  Create an array of days of the week and months.
+*  Write to an alert box "Today is [day of the week], the [date] of [month]"
+*  Change the values in the months array to Spanish
+*  Add to the end of the string: "I'd be happier if it was the [date 10 days later] of [month]"
+*   - Get the contraction right in "I'd" and put the second sentence on a new line in the alert
+**/
+
+
+/**
+*  Create a random number between 4-8
+*  Make a form selector box with 10 choices and using the random number, have that choice in the selector be selected as soon as the page has loaded
+*  Have 7 radio buttons each labeled with a day of the week and under that, a textbox labeled "Your choice is:" When the user selects a radio button, have the textbox echo their choice
+* Add another LONG textbox below that. Create a switch structure that, depending on which radio button was clicked, sends a different string to that textbox.
+**/
+
+
+/**
+* Have a textbox where the user enters their 5 favorite animals.
+* Have a button that reads their entry and converts it to an array of animals.
+* Loop through the array again, writing each to the page on its own line (as a list).
+* Add another loop that changes the animal value to all caps and again creates a list of all but the 3rd and 4th animals using "continue"
+**/
